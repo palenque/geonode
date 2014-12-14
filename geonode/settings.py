5a1b18b3e92c3491c56179d41000efb04af7584b
Gabriel Carmona <gabrielscarmona@gmail.com>
2014-12-14 12:41:00 -0300

    google base

--- geonode/settings.py
+++ geonode/settings.py
@@ -348,7 +348,7 @@ MIDDLEWARE_CLASSES = (
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'guardian.backends.ObjectPermissionBackend',
-    'geonode.security.middleware.ApiKeyAuthentication'
+    # 'geonode.security.middleware.ApiKeyAuthentication'
 )
 
 ANONYMOUS_USER_ID = -1
@@ -696,22 +696,22 @@ API_LIMIT_PER_PAGE = 0
 
 LEAFLET_CONFIG = {
     'TILES': [
-        # Find tiles at:
-        # http://leaflet-extras.github.io/leaflet-providers/preview/
+        # # Find tiles at:
+        # # http://leaflet-extras.github.io/leaflet-providers/preview/
 
-        # Stamen toner lite.
-        ('Watercolor',
-         'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
-         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
-         <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
-         <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
-         <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
-        ('Toner Lite',
-         'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
-         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
-         <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
-         <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
-         <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
+        # # Stamen toner lite.
+        # ('Watercolor',
+        #  'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png',
+        #  'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
+        #  <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
+        #  <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
+        #  <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
+        # ('Toner Lite',
+        #  'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png',
+        #  'Map tiles by <a href="http://stamen.com">Stamen Design</a>, \
+        #  <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; \
+        #  <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
+        #  <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'),
     ],
     'PLUGINS': {
         'esri-leaflet': {
